c02a46257d343fa64886d0381786e00360061431
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2015-12-08 01:14:18 -0200

    Fixado bug em dropdown-menu
    O menu de login funciona bem em sua versão estática pura, mas a versão
    sobre o bootstrap do rails não. O mesmo fechava ao clicar no formulário,
    ação necessária para preenchê-lo. Este bug foi corrigido neste commit.

--- dev/TrocaLivros/app/assets/javascripts/application.js
+++ dev/TrocaLivros/app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+  $('.dropdown-menu').click(function(e) {
+    return e.stopPropagation();
+  });
+});
