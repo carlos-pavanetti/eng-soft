05c33035cbb18860f2a40afdd6743343b457239e
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2015-12-10 13:24:11 -0200

    Refatoração da Home, métodos em Helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,10 @@
 module PagesHelper
+  def link_toggle_accordion(section)
+    link_to section.capitalize, section_results(section),
+            class: 'accordion-toggle', 'data-toggle' => 'collapse'
+  end
+
+  def section_results(section)
+    "##{section}-resultados"
+  end
 end
