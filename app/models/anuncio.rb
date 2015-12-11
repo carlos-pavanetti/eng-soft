806c818c755d346325d0cf055b82758af2e12fa8
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2015-12-11 01:15:36 -0200

    Adicionado escopos em Anuncio

--- app/models/anuncio.rb
+++ app/models/anuncio.rb
@@ -1,4 +1,8 @@
 class Anuncio < ActiveRecord::Base
   has_one :livro
   belongs_to :usuario
+
+  scope :por_tipo, -> (t) { where "tipo_anuncio='#{t}'" }
+  scope :emprestimo, -> { por_tipo('emprestimo') }
+  scope :troca, -> { por_tipo('troca') }
 end
