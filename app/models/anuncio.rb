4a54960802efaa32f6b86df33673eeb88036de2d
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2015-12-12 00:04:57 -0200

    Renderiza label de acordo com o tipo de anúncio

--- app/models/anuncio.rb
+++ app/models/anuncio.rb
@@ -3,6 +3,22 @@ class Anuncio < ActiveRecord::Base
   belongs_to :usuario
 
   scope :por_tipo, -> (t) { where "tipo_anuncio='#{t}'" }
-  scope :emprestimo, -> { por_tipo('emprestimo') }
+  scope :emprestimo, -> { por_tipo('empréstimo') }
   scope :troca, -> { por_tipo('troca') }
+
+  def opcao
+    case tipo_anuncio
+    when 'troca'
+      troco_por
+    when 'emprestimo'
+      prazo_emprestimo.to_s + ' dias'
+    end
+  end
+
+  def troca?
+    return tipo_anuncio == 'troca'
+  end
+  def emprestimo?
+    return tipo_anuncio == 'empréstimo'
+  end
 end
