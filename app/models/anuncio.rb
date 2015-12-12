19b06ca05d0c2d21e0edcf8788666435cdf63301
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2015-12-12 00:27:28 -0200

    Validação de Anúncios

--- app/models/anuncio.rb
+++ app/models/anuncio.rb
@@ -6,6 +6,10 @@ class Anuncio < ActiveRecord::Base
   scope :emprestimo, -> { por_tipo('empréstimo') }
   scope :troca, -> { por_tipo('troca') }
 
+  validates :tipo_anuncio, inclusion: {in: %w(troca empréstimo)}
+  validates :prazo_emprestimo, numericality: {only_integer: true}, :if emprestimo?
+  validates :troco_por, presence: true, :if troca?
+
   def opcao
     case tipo_anuncio
     when 'troca'
