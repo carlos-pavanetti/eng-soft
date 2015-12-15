40654d73c24c4af780e9671bd27fc1256226bd00
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2015-12-15 21:04:37 -0200

    Ajustado bug no cadastro de livros

--- app/helpers/anuncios_helper.rb
+++ app/helpers/anuncios_helper.rb
@@ -8,7 +8,7 @@ module AnunciosHelper
 
     campos << {field: :tipo_anuncio, label: 'Opção', type: :radio_button}
     campos << {field: :observação, label: 'Observação', type: :text_field}
-    campos << {field: :opcao, label: 'Troco Por', type: :text_field}
+    campos << {field: :troco_por, label: 'Troco Por', type: :text_field}
     # campos << {field: :password, label: 'Senha', type: :password_field}
     # campos << {field: :password_confirmation, label: 'Confirme sua senha', type: :password_field}
     # campos << {field: :termos_de_servico, label: 'Aceito os termos de serviço', type: :check_box, require: :signup?}
