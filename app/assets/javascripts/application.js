476a3c5ed7fbbc8632ddabfb8d984d3b4c5329a3
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2015-12-15 12:48:29 -0200

    Cosméticos frontend

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     return e.stopPropagation();
   });
 
-  $(".alert").fadeTo(2000, 500).slideUp(1000, function(){
+  $(".alert").fadeTo(6000, 500).slideUp(1000, function(){
     $(".alert").alert('close');
   });
 
